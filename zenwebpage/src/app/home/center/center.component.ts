fbbef142b01b0558db9fc31bddd13dbf066f33dd
zen <zxu108@yahoo.com>
2018-11-23 19:21:50 -0500

    Updated with review and statistics chart

--- zenwebpage/src/app/home/center/center.component.ts
+++ zenwebpage/src/app/home/center/center.component.ts
@@ -1,4 +1,5 @@
 import { Component, AfterContentInit } from '@angular/core';
+import { Chart } from 'chart.js';
 import { CenterDataService } from './center.service';
 
 @Component({
@@ -9,6 +10,7 @@ import { CenterDataService } from './center.service';
 export class CenterComponent implements AfterContentInit {
   title = 'zenwebbodypage';
   ct: Object;
+  chart = []; // This will hold our chart info
 
   constructor(private data: CenterDataService) { }
 
@@ -19,5 +21,32 @@ export class CenterComponent implements AfterContentInit {
                     console.log(this.ct);
                   }
        );
+
+  
+  console.log('test one');
+  
+  this.chart = new Chart('canvas', {
+  type: 'bar',
+  data: {
+    labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+    datasets: [
+      {
+        label: "Population (millions)",
+        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+        data: [2478,5267,734,784,433]
+      }
+    ]
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
+      text: 'Predicted world population (millions) in 2050'
+    }
   }
+  
+ 
+  
+  });
+  
 }
